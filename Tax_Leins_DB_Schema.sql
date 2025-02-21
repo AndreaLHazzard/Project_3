9c0ae79205a50a0cd9992be989e8e0c2db902819
CoriEsposito <coriesposito1@gmail.com>
2025-02-20 19:02:52 -0500

    added database schema

---
+++ Tax_Leins_DB_Schema.sql
@@ -0,0 +1,26 @@
+create table Leins (
+_id int not null,
+pin varchar not null,
+number int,
+total_amount float,
+primary key (pin)
+);
+
+create table Satisfied_Leins (
+_id int not null,
+pin varchar,
+block_lot varchar,
+filing_date varchar,
+tax_year int,
+dtd varchar,
+lien_description varchar,
+municipality varchar,
+ward float,
+last_docket_entry varchar,
+amount float,
+assignee varchar,
+primary key (_id)
+);
+
+select * from Leins;
+select * from Satisfied_Leins;
